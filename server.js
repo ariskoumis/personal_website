1c16c4a1a70c71df63ad5c8e287ac97c5a9aad76
Aris Koumis <ariskoumis@gmail.com>
2018-01-10 14:55:24 -0800

    Added grunt livereloading

--- server.js
+++ server.js
@@ -4,10 +4,12 @@ var app = express()
 var handles = require(__dirname + "/util/route_handler.js") 
 var settings = require(__dirname + "/util/settings.js")
 
+//Host project on designated local port 
 app.listen(settings.port, function() {
 	console.log(`Hosting on ${settings.port}`)
 })
 
+//Get Request: Home Page
 app.get('/', handles.rootHandler)
 
 app.use(express.static(settings.root))
--- util/route_handler.js
+++ util/route_handler.js
@@ -8,6 +8,26 @@ var options = {
 //A map of all endpoint handlers
 var handle_map = {}
 
+// BEGIN Handler Functions
+/*	
+	@function	rootHandler
+	@parameter	request - the web request object provided by express.js
+	@parameter	response - the web response object provided by express.js
+	@returns	n/a
+	@details 	This function handles all requests for the server root ("/"). Used on a GET request
+*/
+handle_map.rootHandler = function (request, response) {			// GET request on root dir (login page-> index.html)
+	response.set("Content-Type", "text/html");
+	response.sendFile("index.html", options, function (error) {
+		if (error) {
+			logger.log(error);
+			response.status(500).end();
+		} else {
+			logger.log(`Sent index.html to ${settings.port}`);
+			response.end();
+		}
+	});
+};
 handle_map.rootHandler = function(request, response) {
 	response.set("Content-Type", "text/html")
 	response.sendFile("index.html", options, function(error) {
