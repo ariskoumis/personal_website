7fbda3ff1c4f87782132bb1aa92fd3ee6f65a707
Aris Koumis <ariskoumis@gmail.com>
2018-01-23 13:54:52 -0800

    basic mockup of rover model

--- public/js/rover.js
+++ public/js/rover.js
@@ -1,29 +1,32 @@
 var THREE = require('three')
 
-
-
 //objects
-var geometry = new THREE.CylinderGeometry(5, 5, 1, 32);
-var material = new THREE.MeshPhongMaterial({
-	color: 0x156289,
-	emissive: 0x072534,
-	side: THREE.DoubleSide
-});
+var wheel_geometry = new THREE.CylinderGeometry(5, 5, 1, 20);
+var material = new THREE.MeshPhongMaterial({color: 0x156289});
 
-var mesh = new THREE.Mesh(geometry, material);
+var wheel_1 = new THREE.Mesh(wheel_geometry, material);
+wheel_1.position.set(-10, 0, 10);
+wheel_1.rotation.z = Math.PI/2
 
-mesh.position.set(0, 1, 0);
-mesh.updateMatrix();
-mesh.matrixAutoUpdate = false;
+var wheel_2 = new THREE.Mesh(wheel_geometry, material);
+wheel_2.position.set(10, 0, 10);
+wheel_2.rotation.z = Math.PI/2
 
-var mesh2 = new THREE.Mesh(geometry, material);
-mesh2.position.set(0, -31, 0);
-mesh2.rotation.z = Math.PI/2
-mesh2.updateMatrix();
-mesh2.matrixAutoUpdate = false;
+var wheel_3 = new THREE.Mesh(wheel_geometry, material);
+wheel_3.position.set(10, 0, -10);
+wheel_3.rotation.z = Math.PI/2
+
+var wheel_4 = new THREE.Mesh(wheel_geometry, material);
+wheel_4.position.set(-10, 0, -10);
+wheel_4.rotation.z = Math.PI/2
+
+var chasis_geometry = new THREE.BoxGeometry(20, 5, 20);
+var chasis = new THREE.Mesh(chasis_geometry)
+chasis.position.y = 5
+
 
 //add objects to group
 var rover = new THREE.Group();
-rover.add(mesh, mesh2)
+rover.add(wheel_1, wheel_2, wheel_3, wheel_4, chasis)
 
 export default rover
