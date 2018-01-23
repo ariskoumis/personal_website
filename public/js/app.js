7bc797a59c6057e8d27aa9016c6fc18f3f705666
Aris Koumis <ariskoumis@gmail.com>
2018-01-23 13:20:23 -0800

    Objects can now be click+dragged, and began working on rover model

--- public/js/app.js
+++ public/js/app.js
@@ -1,32 +1,99 @@
 //Create a copy of public/index.html to dist/index.html
 require("file-loader?name=index.html!./../index.html");
 
-import React from 'react'
-import ReactDOM from 'react-dom'
-// import Welcome from './welcome.jsx'
-import cube from './cube.js'
-import rover from './rover.js'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import * as THREE from 'three';
+import './TrackballControls.js';
+// import Welcome from './welcome.jsx';
+import cube from './cube.js';
+import rover from './rover.js';
 
-var THREE = require('three')
+var container;
+var camera, controls, scene, renderer;
+var cross;
 
-//setup Three.js Scene
-var scene = new THREE.Scene()
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000)
-camera.position.z = 5
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight)
-document.body.appendChild(renderer.domElement)
+init();
+animate();
 
-scene.add(rover)
+function init() {
+	camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 1, 1000);
+	camera.position.z = 100;
 
-function animate() {
-	requestAnimationFrame(animate)
-	renderer.render(scene, camera)
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
+	controls = new THREE.TrackballControls(camera);
+
+	controls.rotateSpeed = 1.0;
+	controls.zoomSpeed = 1.2;
+	controls.panSpeed = 0.8;
+
+	controls.noZoom = false;
+	controls.noPan = false;
+
+	controls.staticMoving = false;
+	controls.dynamicDampingFactor = 0.3;
+
+	controls.keys = [65, 83, 68];
+
+	controls.addEventListener('change', render);
+
+	//world
+
+	scene = new THREE.Scene();
+	scene.background = new THREE.Color(0xffffff);
+
+	var axis = new THREE.AxesHelper(20);
+	scene.add(axis);
+
+	//objects
+	scene.add(rover);
+
+	//lights
+	var lights = [];
+
+	lights[ 0 ] = new THREE.PointLight( 0xffffff, 1, 0 );
+	lights[ 1 ] = new THREE.PointLight( 0xffffff, 1, 0 );
+	lights[ 2 ] = new THREE.PointLight( 0xffffff, 1, 0 );
+
+	lights[ 0 ].position.set( 0, 200, 0 );
+	lights[ 1 ].position.set( 100, 200, 100 );
+	lights[ 2 ].position.set( - 100, - 200, - 100 );
+
+	scene.add( lights[ 0 ] );
+	scene.add( lights[ 1 ] );
+	scene.add( lights[ 2 ] );
+	
+	
+	//renderer
+
+	renderer = new THREE.WebGLRenderer({ antialias: false });
+	renderer.setPixelRatio(window.devicePixelRatio);
+	renderer.setSize(window.innerWidth, window.innerHeight);
+
+	container = document.getElementById('container');
+	container.appendChild(renderer.domElement);
+
+	//
+	window.addEventListener('resize', onWindowResize, false);
 }
 
-animate()
+function onWindowResize() {
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
 
+	renderer.setPixelRatio(window.devicePixelRatio);
+
+	controls.handleResize;
+
+	render();
+}
+
+function animate() {
+	requestAnimationFrame(animate);
+	controls.update();
+}
+
+function render() {
+	renderer.render(scene, camera);
+}
 // Test to verify if babel and react are functioning as desired.
 // ReactDOM.render(<Welcome/>, document.getElementById('app'));
--- public/js/rover.js
+++ public/js/rover.js
@@ -1,7 +1,29 @@
 var THREE = require('three')
 
-var geometry = new THREE.BoxGeometry(1, 1 , 1)
-var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
-var rover = new THREE.Mesh(geometry, material)
+
+
+//objects
+var geometry = new THREE.CylinderGeometry(5, 5, 1, 32);
+var material = new THREE.MeshPhongMaterial({
+	color: 0x156289,
+	emissive: 0x072534,
+	side: THREE.DoubleSide
+});
+
+var mesh = new THREE.Mesh(geometry, material);
+
+mesh.position.set(0, 1, 0);
+mesh.updateMatrix();
+mesh.matrixAutoUpdate = false;
+
+var mesh2 = new THREE.Mesh(geometry, material);
+mesh2.position.set(0, -31, 0);
+mesh2.rotation.z = Math.PI/2
+mesh2.updateMatrix();
+mesh2.matrixAutoUpdate = false;
+
+//add objects to group
+var rover = new THREE.Group();
+rover.add(mesh, mesh2)
 
 export default rover
